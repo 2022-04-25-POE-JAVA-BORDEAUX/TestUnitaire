cbfd49385e47eaa8511f8cd75649110daa13f21c
jcd-formateur <jcd.pro+formateur@gmail.com>
2022-06-07 12:09:28 +0200

    Ajout 2e Test unitaire

--- src/test/java/com/poe/demotestunitaire/calculator/CalculatorTest.java
+++ src/test/java/com/poe/demotestunitaire/calculator/CalculatorTest.java
@@ -7,12 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
     
     @Test
-    public void testAdd() {
+    public void testAdd3plus2retourne5() {
         Calculator calculator = new Calculator();
         
-        int result = calculator.add(1, 2);
+        int result = calculator.add(3, 2);
+        
+        assertEquals(5, result);
+    }
+    
+    @Test
+    public void testAdd2plus1retourne3() {
+        Calculator calculator = new Calculator();
+        
+        int result = calculator.add(2, 1);
         
         assertEquals(3, result);
     }
-    
 }
