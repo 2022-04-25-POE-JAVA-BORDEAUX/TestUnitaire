2a32be9a840ad981b4f9c806a4de92ae8f33989e
jcd-formateur <jcd.pro+formateur@gmail.com>
2022-06-08 15:19:57 +0200

    Exercice Le Distributeur: à compléter

---
+++ src/main/java/distributeur/Distributeur.java
@@ -0,0 +1,27 @@
+package distributeur;
+
+import java.util.ArrayList;
+
+public class Distributeur {
+    
+    int credit; // argent dispo pour acheter
+    
+    ArrayList<Produit> stock = new ArrayList<>();
+    
+    public void remplirLeStock() {
+        
+    }
+    
+    public void insererArgent(int montant){
+        
+    }
+    
+    public void commanderProduit(int indexProduit){
+        
+    }
+    
+    public void recupererArgentRestant() {
+        
+    }
+    
+}
---
+++ src/main/java/distributeur/Produit.java
@@ -0,0 +1,8 @@
+package distributeur;
+
+public class Produit {
+    
+    String nom;
+    int prix;
+    int quantite;
+}
