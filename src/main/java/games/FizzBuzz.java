dbd40a4e5a579e97865f7e9b57d559d78143d3fe
jcd-formateur <jcd.pro+formateur@gmail.com>
2022-06-08 15:01:22 +0200

    Exercice FizzBuzz terminé

--- src/main/java/games/FizzBuzz.java
+++ src/main/java/games/FizzBuzz.java
@@ -4,6 +4,14 @@ public class FizzBuzz {
     
     public static String play(int number){
         
-        return "";
+        //if(number % 3 == 0 && number % 5 == 0)
+        if(number % 15 == 0)
+            return "FizzBuzz";
+        if(number  % 3 == 0)
+            return "Fizz";
+        if(number  % 5 == 0)
+            return "Buzz";
+        
+        return ""+number;
     }
 }
--- src/test/java/com/poe/demotestunitaire/games/FizzBuzzTest.java
+++ src/test/java/com/poe/demotestunitaire/games/FizzBuzzTest.java
@@ -1,11 +1,30 @@
 package com.poe.demotestunitaire.games;
 
+import games.FizzBuzz;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FizzBuzzTest {
     
-
+    @Test
+    public void testFizzBuzz15() {
+        assertEquals("FizzBuzz", FizzBuzz.play(15));
+    }
+    
+    @Test
+    public void testFizzBuzz3() {
+        assertEquals("Fizz", FizzBuzz.play(3));
+    }
+    
+     @Test
+    public void testFizzBuzz5() {
+        assertEquals("Buzz", FizzBuzz.play(5));
+    }
+    
+     @Test
+    public void testFizzBuzz2() {
+        assertEquals("2", FizzBuzz.play(2));
+    }
     
 }
