da97baa8ea3ea87878df03cd09c9cb4888e2abd3
jcd-formateur <jcd.pro+formateur@gmail.com>
2022-06-08 16:52:51 +0200

    Creation Calculator dans BeforeAll à la place de BeforeEach. dans CalculatorTest

--- src/test/java/com/poe/demotestunitaire/calculator/CalculatorTest.java
+++ src/test/java/com/poe/demotestunitaire/calculator/CalculatorTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,11 +12,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
     
-    Calculator calculator;
+    static Calculator calculator;
     
-    @BeforeEach
-    public void setup(){
-       calculator  = new Calculator();
+    @BeforeAll
+    public static void setup(){
+       calculator = new Calculator();
     }
     
     @Test
