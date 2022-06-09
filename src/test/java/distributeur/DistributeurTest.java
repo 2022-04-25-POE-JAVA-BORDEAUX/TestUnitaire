83c666a7aff91bb594f0a5f6d10b2934857e0227
jcd-formateur <jcd.pro+formateur@gmail.com>
2022-06-09 12:31:22 +0200

    nouveaux Tests Unitaires pour commanderProduit()

--- src/test/java/distributeur/DistributeurTest.java
+++ src/test/java/distributeur/DistributeurTest.java
@@ -37,4 +37,54 @@ public class DistributeurTest {
         assertEquals(4, distributeur.getCredit());
         assertEquals(4, distributeur.getProduit(1).getQuantite());
     } 
+    
+    @Test
+    public void testCommanderProduitProduitInexistant(){
+        Distributeur distributeur = new Distributeur();
+        distributeur.remplirLeStock();
+        distributeur.insererArgent(5);
+        
+        distributeur.commanderProduit(0);
+        
+        assertEquals(5, distributeur.getCredit());
+        assertEquals(5, distributeur.getProduit(1).getQuantite());
+        assertEquals(5, distributeur.getProduit(2).getQuantite());
+        assertEquals(5, distributeur.getProduit(3).getQuantite());
+    }
+    
+    @Test
+    public void testCommanderProduitStockVide(){
+        Distributeur distributeur = new Distributeur();
+        distributeur.remplirLeStock();
+        distributeur.getProduit(1).setQuantite(0);
+        distributeur.insererArgent(5);
+        
+        distributeur.commanderProduit(1);
+        
+        assertEquals(5, distributeur.getCredit());
+        assertEquals(0, distributeur.getProduit(1).getQuantite());
+    }
+    
+    @Test
+    public void testCommanderProduitCreditZero(){
+        Distributeur distributeur = new Distributeur();
+        distributeur.remplirLeStock();
+        
+        distributeur.commanderProduit(1);
+        
+        assertEquals(0, distributeur.getCredit());
+        assertEquals(5, distributeur.getProduit(1).getQuantite());
+    }
+    
+    @Test
+    public void testCommanderProduitCreditInsuffisant(){
+        Distributeur distributeur = new Distributeur();
+        distributeur.remplirLeStock();
+        distributeur.insererArgent(1);
+        
+        distributeur.commanderProduit(2);
+        
+        assertEquals(1, distributeur.getCredit());
+        assertEquals(5, distributeur.getProduit(2).getQuantite());
+    }
 }
